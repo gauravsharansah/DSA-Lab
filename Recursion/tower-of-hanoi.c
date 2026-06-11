d76b8b78f4498fceb6bc1d9ea871d06f871e9898
Gaurav Sharan Kumar <shambhu1gaurabh@gmail.com>
2026-06-11 14:51:39 +0545

    tower of hanoi problem solved

---
+++ Recursion/tower-of-hanoi.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void move(int n,char s, char a, char d);
+int main()
+{
+    int rings;
+    printf("Enter no of rings in source: ");
+    scanf("%d",&rings);
+    move(rings,'A','B','C');
+    return 0;
+}
+void move(int n,char s, char a, char d)
+{
+    if(n==1)
+    {
+        printf("\nMove Disk1 from rod %c to rod %c",s,d);
+        return;
+    }
+    move(n-1,s,d,a);
+    printf("\nMove Disk%d from rod %c to rod %c",n,s,d);
+    move(n-1,a,s,d);
+}
